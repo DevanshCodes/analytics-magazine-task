9ce51f3525739606e6de303bad3beced6c2f31ff
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-07-22 22:42:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import machineLogo from "./machineLogo.png";
+import more from "./more.svg";
+import loupe from "./loupe.svg";
+import "./App.css";
+import { Nav, Form, InputGroup } from "react-bootstrap";
+import Avatar from "@material-ui/core/Avatar";
+import { FiSearch } from "react-icons/fi";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import machineLogo from "../machineLogo.png";
+import more from "../more.svg";
+import bellicon from "../bellicon.png";
+import settings from "../settings.png";
+
+import { Nav, Form, InputGroup, NavDropdown } from "react-bootstrap";
+import Avatar from "@material-ui/core/Avatar";
+import { FiSearch } from "react-icons/fi";
+import "./navbar.scss";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        className="navreact"
+      >
+        <Nav.Item>
+          <img src={machineLogo} className="App-logo" alt="logo" />
+        </Nav.Item>
+        <Nav.Item>
+          <img src={more} className="more" alt="logo" />
+        </Nav.Item>
+        <Nav.Item className="searchbox">
+          <input
+            type="text"
+            name="whatever"
+            id="search"
+            className="search"
+            placeholder="Type Here for Search"
+          />
+        </Nav.Item>
+        <Nav.Item>
+          <div className="users">
+            <div className="user">
+              <Avatar className="avatar">R</Avatar>
+            </div>
+            <NavDropdown title="randomuser" id="collasible-nav-dropdown">
+              <NavDropdown.Item>
+                <div>Logout</div>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+        </Nav.Item>
+        <Nav.Item>
+          <img src={bellicon} className="bellicon" alt="logo" />
+        </Nav.Item>
+        <Nav.Item>
+          <img src={settings} className="bellicon" alt="logo" />
+        </Nav.Item>
+      </Nav>
+    );
+  }
+}
+
+export default Navbar;
