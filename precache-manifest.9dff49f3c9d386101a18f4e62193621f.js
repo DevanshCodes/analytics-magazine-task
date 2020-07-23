12eb0ab1a8b378e75b09db36a42a3b4a2832f4e4
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-07-23 18:31:11 +0530

    Updates

---
+++ precache-manifest.9dff49f3c9d386101a18f4e62193621f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d94bdd0e7df8e16d6f2b702912b227",
+    "url": "/analytics-magazine-task/index.html"
+  },
+  {
+    "revision": "1a57a8537319e54a731a",
+    "url": "/analytics-magazine-task/static/css/main.ce9613ce.chunk.css"
+  },
+  {
+    "revision": "920215e72773e022b477",
+    "url": "/analytics-magazine-task/static/js/2.6dbe8acc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/analytics-magazine-task/static/js/2.6dbe8acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a57a8537319e54a731a",
+    "url": "/analytics-magazine-task/static/js/main.b4cd6ff4.chunk.js"
+  },
+  {
+    "revision": "84d72fae78c74777f0ad",
+    "url": "/analytics-magazine-task/static/js/runtime-main.2783c998.js"
+  },
+  {
+    "revision": "a8b9dfd1801bfa1a8abd82923819c65e",
+    "url": "/analytics-magazine-task/static/media/headerImage2.a8b9dfd1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/analytics-magazine-task/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b2feb1c702b9b4c46343cfc222770c0d",
+    "url": "/analytics-magazine-task/static/media/loupe.b2feb1c7.svg"
+  },
+  {
+    "revision": "825eb1b9ade8b793fd8f901d66367aad",
+    "url": "/analytics-magazine-task/static/media/more.825eb1b9.svg"
+  }
+]);
